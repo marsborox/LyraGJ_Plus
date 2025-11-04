f114eaa78be8ea00c083e5e51bf4f7970baa4b24
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-11-04 19:04:59 +0100

    some work on tilemaps

---
+++ Assets/Scripts/ScriptableObjects/Action_SO/Action_SO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+//[CreateAssetMenu(fileName = "Action_SO", menuName = "Scriptable Objects/Action_SO")]
+public class Action_SO : ScriptableObject
+{
+    public static void PerformAction(Unit source, Unit target)
+    { 
+        
+    }
+}
