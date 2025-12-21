5e9b75f40ec4e20bdff9c5b765fe23499cf257b1
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-12-21 21:20:33 +0100

    UI minor rework

---
+++ Assets/Scripts/UI/HealthIndicator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthIndicator_UI : MonoBehaviour
+{
+    [SerializeField] private Image _healthBar;
+    [SerializeField] private Player _player;
+    public void Update()
+    {
+        HealthBarFill();
+    }
+
+    void HealthBarFill()
+    {
+        float healthFraction = (float)_player.ReturnHealthCurrent() / (float)_player.ReturnHealthMax();
+        _healthBar.fillAmount = healthFraction;
+        Debug.Log("fillingHealthInUI");
+    }
+}
