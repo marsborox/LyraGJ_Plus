e75a1197c9be7cc3ce14eeb6b65b56e2ebbe0353
mierka_m <162184787+marsborox@users.noreply.github.com>
2026-03-29 23:07:38 +0200

    checking program and

--- Assets/Scripts/Units/PlayerCombat.cs
+++ Assets/Scripts/Units/PlayerCombat.cs
@@ -65,13 +65,13 @@ public class PlayerCombat : UnitCombat
     public override void TakeDamage(int damage)
     {
         isAttacking = false;
-
         base.TakeDamage(damage);
 
         MySoundManager.instance.PlayEnemyHit();
         if (healthCurrent <= 0)
         {
             Die();
+            //Debug.Log("player died");
         }
     }
     public override void PostAttackAnimationEventUnit()
