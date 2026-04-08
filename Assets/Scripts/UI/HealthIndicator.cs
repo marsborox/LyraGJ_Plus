4e8d823a7c935e7c26e2f777ad81853137003727
mierka_m <162184787+marsborox@users.noreply.github.com>
2026-04-08 22:58:47 +0200

    healthBar fix
    health bar fills percentage of hearth based on min and max fill space of fill img

--- Assets/Scripts/UI/HealthIndicator.cs
+++ Assets/Scripts/UI/HealthIndicator.cs
@@ -5,13 +5,29 @@ public class HealthIndicator_UI : MonoBehaviour
 {
     [SerializeField] private Image _healthBar;
     [SerializeField] private Player _player;
+    [SerializeField]public float _minHealthImageFill=0.11f;
+    [SerializeField]public float _maxHealthImageFill=0.84f;
     public void Update()
     {
-        HealthBarFill();
+        //HealthBarFill();
+        CalcFillAmountRelative();
     }
 
+    void CalcFillAmountRelative()
+    {
+        float realRange = _maxHealthImageFill - _minHealthImageFill;
+        //trojclenka relative  fill fraction as whole isnt 1
+
+        float relativeFraction = (float)_player.ReturnHealthCurrent() / (float)_player.ReturnHealthMax();
+        //add min value
+        float realFraction = _minHealthImageFill + (relativeFraction*realRange);
+        Debug.Log("range= "+realRange+" fraction= "+realFraction);
+        _healthBar.fillAmount = realFraction;
+    }
     void HealthBarFill()
     {
+
+
         float healthFraction = (float)_player.ReturnHealthCurrent() / (float)_player.ReturnHealthMax();
         _healthBar.fillAmount = healthFraction;
         //Debug.Log("fillingHealthInUI");
