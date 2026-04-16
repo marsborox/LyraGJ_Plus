c291c50b17485f03c740e85e70025f0f1a5ed38c
Peter Duncak <peter.duncak@gmail.com>
2026-04-16 15:04:52 +0200

    go back to menu once credits finishes

--- Assets/Scripts/misc/HandleCreditsScene.cs
+++ Assets/Scripts/misc/HandleCreditsScene.cs
@@ -1,13 +1,35 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Video;
 
 public class HandleCreditsScene : MonoBehaviour
 {
+    [SerializeField] private VideoPlayer videoPlayer;
+    
     void Update()
     {
         if (Keyboard.current.anyKey.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame)
         {
             MySceneManager.instance.OpenScene(GameScene.MAIN_MENU);
         }
+    }
+    void OnEnable()
+    {
+        videoPlayer.loopPointReached += OnVideoFinished;
+        
+        videoPlayer.Stop();
+        videoPlayer.time = 0;
+        videoPlayer.Play();
+    }
+
+    void OnDisable()
+    {
+        videoPlayer.loopPointReached -= OnVideoFinished;
+        videoPlayer.Stop();
+    }
+
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        MySceneManager.instance.OpenScene(GameScene.MAIN_MENU);
     }
 }
